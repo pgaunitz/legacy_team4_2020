8188e8e03f0858fa25d263ae57a8f59606df9db4
Jaime <jaime.cruz.hostalot@gmail.com>
2020-02-12 13:17:03 +0100

    Added test for, see name, see email pasword and log in

--- spec/Feature/User_can_create_account.rb
+++ spec/Feature/User_can_create_account.rb
@@ -7,8 +7,26 @@ feature 'Check if user can create account' do
         visit new_user_registration_path
     end
 
-    it 'displays if user can see password' do
+    it 'User can see password' do
         expect(page).to have_content 'Password'
     end
+
+    it 'User can see name' do
+        expect(page).to have_content 'Name'
+    end
+
+    it 'User can see Email' do
+        expect(page).to have_content 'Email'
+    end
+
+    it 'User can see Password confirmation' do
+        expect(page).to have_content 'Password confirmation'
+    end
+
+    it 'User can see log in, in sign up' do
+        expect(page).to have_content 'Log in'
+    end
+
+    
 
 end
