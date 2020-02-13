77d82a7f1b8565fe88a8e84c953d99fbcbc29e32
Philip Gaunitz <fluffen@pgaunitz.se>
2020-02-13 17:19:58 +0100

    User can log in successfully

--- spec/features/user_can_login_to_their_account_spec.rb
+++ spec/features/user_can_login_to_their_account_spec.rb
@@ -1,23 +1,26 @@
 require './spec/user.rb'
 
 feature 'Check if user can login to their account' do
-    let(:registered_user) { FactoryBot.create(:user) }
+    let(:registered_user) { FactoryBot.create(:user) }  
     context 'user is in login page'
 
     before do
 
-        visit user_session_path
+        visit root_path
+        click_on 'Login'
     
     end
 
     describe 'User can log in successfully' do
-                
-        fill_in 'Email', with: :registered_user.email
-        fill_in 'Password', with: :registered_user.password
+            
+        
+        it 'User can login' do
+        fill_in 'Email', with: registered_user.email
+        fill_in 'Password', with: registered_user.password
         click_on 'Log in'
         
-        it 'User can see welcome index page' do
-        expect(page).to have_content 'Craft Academy Mailboxer'
+        
+        expect(page).to have_content 'Signed in successfully.'
         end
 
     end
