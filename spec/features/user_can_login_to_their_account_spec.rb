4ee88a567f694030e516b637a7d538dad25f908e
Karolina Frostare <karolina.frostare@gmail.com>
2020-02-13 16:42:02 +0100

    update syntax error in rails_helper file for tests

--- spec/features/user_can_login_to_their_account_spec.rb
+++ spec/features/user_can_login_to_their_account_spec.rb
@@ -1,23 +1,23 @@
-require 'spec/factories/users.rb'
+require './spec/user.rb'
 
 feature 'Check if user can login to their account' do
-    let(:registered_user) {  FactoryBot.create(:user)}
+    let(:registered_user) { FactoryBot.create(:user) }
     context 'user is in login page'
 
     before do
+
         visit user_session_path
-        describe 'User can log in successfully' do
+    
+    end
+
+    describe 'User can log in successfully' do
                 
-                visit root_path 
-                click_on 'Login'
-                fill_in 'Email', with: user.email
-                fill_in 'Password', with: user.password
-                click_on 'Log in'
-
-            it 'User can see welcome index page' do
-                expect(page).to have_content 'Craft Academy Mailboxer'
-            end
-
+        fill_in 'Email', with: :registered_user.email
+        fill_in 'Password', with: :registered_user.password
+        click_on 'Log in'
+        
+        it 'User can see welcome index page' do
+        expect(page).to have_content 'Craft Academy Mailboxer'
         end
 
     end
