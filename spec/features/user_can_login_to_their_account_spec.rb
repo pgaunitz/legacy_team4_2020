d10c1b6e4f83740a37006e648ac05bb6fe16c8aa
Karolina Frostare <karolina.frostare@gmail.com>
2020-02-13 16:01:30 +0100

    update user_can_login_to_their_account_spec.rb with FactoryBot variables

--- spec/features/user_can_login_to_their_account_spec.rb
+++ spec/features/user_can_login_to_their_account_spec.rb
@@ -1,47 +1,25 @@
 require 'spec/factories/users.rb'
 
 feature 'Check if user can login to their account' do
-    let(:registred_user) {  FactoryBot.create(:user)}
+    let(:user) {  FactoryBot.create(:user :name "James", :email "user@mail.com", :password "password")}
     context 'user is in login page'
 
     before do
         visit new_user_session_path
-    end
+        describe 'User can log in successfully' do
+                
+                visit root_path 
+                click_on 'Login'
+                fill_in 'Email', with: user.email
+                fill_in 'Password', with: user.password
+                click_on 'Log in'
 
-    it 'User can see log in' do
-        expect(page).to have_content 'Log in'
-    end
-    it 'User can see email' do
-        expect(page).to have_content 'Email'
-    end
-    it 'User can see password' do
-        expect(page).to have_content 'Password'
-    end
-    it 'User can see remember me' do
-        expect(page).to have_content 'Remember me'
-    end
-    it 'User can see Sign up' do
-        expect(page).to have_content 'Sign up'
-    end
-    it 'User can see Forgot your password' do
-        expect(page).to have_content 'Forgot your password?'
-    end
+            it 'User can see welcome index page' do
+                expect(page).to have_content 'Craft Academy Mailboxer'
+            end
 
-    #can fill in form
-
-    describe 'User can log in using email and password' do
-        before do
-            fill_in 'Email', with: 'test@email.com'
-            fill_in 'Password', with: '12345678'
-
-            click_on 'Log in'
         end
 
-        it 'User can see welcome index page' do
-            expect(page).to have_content 'Craft Academy Mailboxer'
-        end
-    end 
-
-
+    end
 
 end
