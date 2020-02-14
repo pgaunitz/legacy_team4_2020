ddecc10a490e80049b9c994cd48e24f63248905b
Karolina Frostare <karolina.frostare@gmail.com>
2020-02-14 12:26:05 +0100

    Update userCanLoginToTheirAccount rspec test with sad paths such as tries to login with no credentials, just clicks login button tries to login with no @ in the email

--- spec/features/user_can_login_to_their_account_spec.rb
+++ spec/features/user_can_login_to_their_account_spec.rb
@@ -10,16 +10,34 @@ feature 'Check if user can login to their account' do
         click_on 'Login'
     end
 
-    describe 'User can log in successfully' do
-        let(:registered_user) { FactoryBot.create(:user) }
-        it 'User can see welcome index page' do
+    describe 'User can log in - happy path' do
+        # let(:registered_user) { FactoryBot.create(:user) } --- commenting out this code as it is repeated above.2020
+        it 'User gets through to welcome index page' do
         fill_in 'Email', with: registered_user.email
         fill_in 'Password', with: registered_user.password
         click_on 'Log in'
 
         expect(page).to have_content 'Signed in successfully'
         end
+    end
+
+    describe 'User is denied to login - sad path' do
+        it 'Displays error message - Invalid Email or password.' do
+        fill_in 'Email', with: nil
+        fill_in 'Password', with: nil
+        click_on 'Log in'
+        expect(page).to have_content 'Invalid Email or password.'
+        end
+
+        it 'Displays error message - Forgot your password?' do
+            fill_in 'Email', with: 'usermail.com'
+            fill_in 'Password', with: registered_user.password
+            click_on 'Log in'
+            expect(page).to have_content 'Forgot your password?'
+        end
 
     end
+
+
 
 end
