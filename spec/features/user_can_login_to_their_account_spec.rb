fedd81add570806110d56f9f23f74c1c6c34f495
Karolina Frostare <karolina.frostare@gmail.com>
2020-02-14 11:37:49 +0100

    erase db and update rspec tests

--- spec/features/user_can_login_to_their_account_spec.rb
+++ spec/features/user_can_login_to_their_account_spec.rb
@@ -2,22 +2,22 @@ require './spec/user.rb'
 
 feature 'Check if user can login to their account' do
     let(:registered_user) { FactoryBot.create(:user) }
+
     context 'user is in login page'
 
     before do
-
-        visit user_session_path
-    
+        visit root_path
+        click_on 'Login'
     end
 
     describe 'User can log in successfully' do
-                
-        fill_in 'Email', with: :registered_user.email
-        fill_in 'Password', with: :registered_user.password
-        click_on 'Log in'
-        
+        let(:registered_user) { FactoryBot.create(:user) }
         it 'User can see welcome index page' do
-        expect(page).to have_content 'Craft Academy Mailboxer'
+        fill_in 'Email', with: registered_user.email
+        fill_in 'Password', with: registered_user.password
+        click_on 'Log in'
+
+        expect(page).to have_content 'Signed in successfully'
         end
 
     end
